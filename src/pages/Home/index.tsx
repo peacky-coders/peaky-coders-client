0853bed0e7c1efb10427e2a04c53ad5b52c22e89
YaroslavWeb <yaroslavweb98@gmail.com>
2023-02-08 19:55:39 +0800

    instal ky

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,9 @@
+import { useQuery } from '@tanstack/react-query'
+
 import { t } from 'languages'
 
 export const Home = () => {
+  const {} = useQuery()
+
   return <div>{t('page.home.title')}</div>
 }
