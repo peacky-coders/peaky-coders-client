2b29128e17003f662d28fe8b579c48aa111fa1ba
YaroslavWeb <yaroslavweb98@gmail.com>
2023-03-02 22:06:50 +0800

    update editor

--- src/pages/Editor/index.tsx
+++ src/pages/Editor/index.tsx
@@ -1,7 +1,18 @@
+import MDEditor from '@uiw/react-md-editor'
+import { useState } from 'react'
+
+import * as S from './styles'
+
 export const Editor = () => {
+  const [editorValue, setEditorValue] = useState('')
+
+  const handleChangeEditorValue = (value: string | undefined) => {
+    setEditorValue(value || '')
+  }
+
   return (
-    <div>
-      <span>Здесь будут поля для редактирования</span>
-    </div>
+    <S.Wrapper>
+      <MDEditor height={800} value={editorValue} onChange={handleChangeEditorValue} />
+    </S.Wrapper>
   )
 }
